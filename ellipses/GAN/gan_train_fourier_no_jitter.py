63a4d55d33a41d72832a2003f6a74101827f4f04
Johan Fredrik Agerup <johanfag@nam-shub-02.uio.no>
2024-01-22 11:30:33 +0100

    added the last files that was untracked

---
+++ ellipses/GAN/gan_train_fourier_no_jitter.py
@@ -0,0 +1,187 @@
+"""
+DESCRIPTION OF WHAT THIS SCRIPT DOES
+------------------------------------
+Terms
+    - jitter, means that noise, typically Gaussian, is added to the data while training to reduce overfit
+"""
+
+
+import os
+
+import matplotlib as mpl
+import torch
+import torchvision
+
+# vegard's implementations
+from data_management import IPDataset, ToComplex, SimulateMeasurements
+from networks import Generator, Discriminator
+from operators import Fourier as Fourier
+from operators import Fourier_matrix as Fourier_m
+from operators import (
+    RadialMaskFunc,
+)
+
+
+# ----- load configuration -----
+import config
+
+# ----- global configuration -----
+mpl.use("agg")
+device = torch.device("cpu")
+# if GPU availablei
+gpu_avail = torch.cuda.is_available()
+if gpu_avail:
+    device = torch.device("cuda:0")
+    torch.cuda.set_device(device)
+
+# ----- measurement configuration -----
+mask_func = RadialMaskFunc(config.n, 40)
+mask = mask_func((1,) + config.n + (1,))
+mask = mask.squeeze(-1)
+mask = mask.unsqueeze(1)
+# Fourier matrix
+OpA_m = Fourier_m(mask)
+# Fourier operator
+OpA = Fourier(mask)
+# set device for operators
+OpA_m.to(device)
+
+# ----- network configuration -----
+from config_gan import *
+from dataclasses import asdict
+generator_params = asdict(Generator_params())
+discriminator_params = asdict(Discriminator_params())
+
+# ------ Definition of objective -------------- 
+# Binary Cross Entropy between the target and the input probabilities
+# BCELoss(weight=None, size_average=None, reduce=None, reduction='mean')
+# doc : https://pytorch.org/docs/stable/generated/torch.nn.BCELoss.html
+adversarial_loss_func = torch.nn.BCELoss()
+adversarial_loss_func.cuda()
+
+# Set training parameters
+gan_train_params = GAN_train_params()
+gan_train_params.num_epochs = 20
+train_params = asdict( gan_train_params )
+
+# ----- data configuration -----
+train_data_params = {
+    "path": config.DATA_PATH,
+}
+train_data = IPDataset
+
+val_data_params = {
+    "path": config.DATA_PATH,
+}
+val_data = IPDataset
+
+# ------ save hyperparameters -------
+os.makedirs(train_params["save_path"][-1], exist_ok=True)
+with open(
+    os.path.join(train_params["save_path"][-1], "hyperparameters.txt"), "w"
+) as file:
+    for key, value in generator_params.items():
+        file.write(key + ": " + str(value) + "\n")
+    for key, value in discriminator_params.items():
+        file.write(key + ": " + str(value) + "\n")
+    for key, value in train_params.items():
+        file.write(key + ": " + str(value) + "\n")
+    for key, value in train_data_params.items():
+        file.write(key + ": " + str(value) + "\n")
+    for key, value in val_data_params.items():
+        file.write(key + ": " + str(value) + "\n")
+    file.write("train_phases" + ": " + str(train_params["train_phases"]) + "\n")
+
+# ------ construct network and train -----
+# TODO make the model work and make more streamlined later on
+generator = Generator(**generator_params)
+# parameter check holds if all params are on the same device
+if next(generator.parameters()).device == torch.device("cpu"):
+    generator = generator.to(device)
+discriminator = Discriminator(**discriminator_params)
+if next(discriminator.parameters()).device == torch.device("cpu"):
+    discriminator = discriminator.to(device)
+assert next(generator.parameters()).device == device and next(discriminator.parameters()).device == device, "the G and D has not yet been set to proper device"
+# get train and validation data
+# data has shape (number of samples, (measurements, images) )
+# Note that the second dimension consist of a 2-tuple
+# image x has shape (2, N, N), since x in C^{N x N}
+# measurement y has shape (2, m) since y in C^m
+train_data = train_data("train", **train_data_params)
+val_data = val_data("val", **val_data_params)
+#set transforms
+train_data.transform = train_params["train_transform"]
+val_data.transform = train_params["val_transform"]
+# initialize data loaders
+data_load_train = torch.utils.data.DataLoader(
+    train_data, train_params["batch_size"], **train_params["train_loader_params"]
+)
+data_load_val = torch.utils.data.DataLoader(
+    val_data, train_params["batch_size"], **train_params["val_loader_params"]
+)
+
+# optimizer setup
+optimizer_G = train_params["optimizer_G"](generator.parameters(),     **train_params["optimizer_G_params"]) 
+optimizer_D = train_params["optimizer_D"](discriminator.parameters(), **train_params["optimizer_D_params"]) 
+scheduler_G = train_params["scheduler_G"](optimizer_G, **train_params["scheduler_G_params"]) 
+scheduler_D = train_params["scheduler_D"](optimizer_D, **train_params["scheduler_D_params"])
+
+# log setup
+import pandas as pd
+logging = pd.DataFrame(
+        columns=["generator_loss", "discriminator_loss", "lr_generator", "lr_discriminator", "mem_alloc"]
+)
+# progressbar setup see training loop
+#from tqdm import tqdm
+
+# load parameters
+#generator.load_state_dict(torch.load(train_params["save_path"] + "/generator_no_jitter_epoch90.pth") )
+#discriminator.load_state_dict(torch.load(train_params["save_path"] + "/discriminator_no_jitter_epoch90.pth") )
+from importlib import reload
+import trainer_gan; reload(trainer_gan)
+from trainer_gan import *
+generator, discriminator, logging = train_loop_gan(
+    train_params          = train_params,
+    generator_params      = generator_params,
+    generator             = generator,
+    discriminator         = discriminator,
+    data_load_train       = data_load_train,
+    device                = device,
+    optimizer_G           = optimizer_G,
+    optimizer_D           = optimizer_D,
+    scheduler_G           = scheduler_G,
+    scheduler_D           = scheduler_D,
+    adversarial_loss_func = adversarial_loss_func,
+    logging               = logging,
+    wgan                  = True,
+    ncritic               = 5,
+    jitter                = False,
+    fn_suffix             = "_wgan_no_jitter",
+    gp_coeff              = 10,
+) 
+
+# TODO: implement optimization over latent vector z
+# log z optimisation
+logging_zoptim = pd.DataFrame(
+        columns=["objective", "measurement_error", "representation_error", "lr", "mem_alloc"]
+)
+
+# load CS train parameters
+cs_gan_train_params = asdict( CS_GAN_train_params() )
+
+# init generator input
+init_shape = (cs_gan_train_params["batch_size"], generator_params["latent_dim"])
+z_init = torch.randn(init_shape).to(device)
+
+# optimize generator input
+#breakpoint()
+z_optim, logging = optimize_generator_input_vector(
+    cs_train_params      = cs_gan_train_params,
+    z_init               = z_init,
+    generator            = generator,
+    data_load_train      = data_load_train,
+    measurement_operator = OpA,
+    logging              = logging_zoptim,
+    device               = device, 
+)
+
---
+++ ellipses/script_train_fourier_unet_no_jitter.py
@@ -0,0 +1,179 @@
+"""
+Training script for supervised learning using a UNet.
+UNet is implemented in networks.py.
+-----------------------------------------------------
+Terms
+    - jitter, means that noise, typically Gaussian, is added to the data while training to reduce overfit
+"""
+
+
+import os
+
+import matplotlib as mpl
+import torch
+import torchvision
+
+from data_management import IPDataset, SimulateMeasurements, ToComplex
+from networks import UNet
+from operators import Fourier as Fourier
+from operators import Fourier_matrix as Fourier_m
+from operators import (
+    LearnableInverterFourier,
+    RadialMaskFunc,
+    MaskFromFile,
+)
+
+
+# ----- load configuration -----
+import config  # isort:skip
+
+# ----- global configuration -----
+mpl.use("agg")
+device = torch.device("cpu")
+# if GPU availablei
+gpu_avail = torch.cuda.is_available()
+if gpu_avail:
+    device = torch.device("cuda:0")
+    torch.cuda.set_device(device)
+
+# ----- measurement configuration -----
+""" Radial sampling golden 180 angle
+mask_func = RadialMaskFunc(config.n, 40)
+mask = mask_func((1,) + config.n + (1,))
+mask = mask.squeeze(-1)
+mask = mask.unsqueeze(1)
+"""
+mask_fromfile = MaskFromFile(
+    path = os.getcwd() + "/sampling_patterns/",
+    filename = "multilevel_sampling_pattern_sr2.500000e-01_a2_r0_2_levels50.png"
+)
+mask = mask_fromfile.mask[None]
+# Fourier matrix
+OpA_m = Fourier_m(mask)
+# Fourier operator
+OpA = Fourier(mask)
+# init learnable inversion operator
+inverter = LearnableInverterFourier(config.n, mask, learnable=False)
+# set device for operators
+OpA_m.to(device)
+inverter.to(device)
+
+# ----- network configuration -----
+unet_params = {
+    "in_channels"   : 2,
+    "drop_factor"   : 0.0,
+    "base_features" : 32,
+    "out_channels"  : 2,
+    "operator"      : OpA_m,
+    "inverter"      : inverter,
+}
+unet = UNet
+# ----- training configuration -----
+mseloss = torch.nn.MSELoss(reduction="sum")
+
+def loss_func(pred, tar):
+    return (
+        mseloss(pred, tar) / pred.shape[0]
+    )
+
+
+train_phases = 2
+train_params = {
+    #"num_epochs": [100, 10], # fastmri, single-coil
+    "num_epochs" : [35,6],
+    #"batch_size": [10, 10], # fastmri single-coil
+    "batch_size": [10, 10], # ellipses
+    "loss_func": loss_func,
+    "save_path": [
+        os.path.join(
+            config.RESULTS_PATH,
+            "/supervised/circ_sr0.25/Fourier_UNet_no_jitter_ellipses_256"
+            "train_phase_{}".format((i + 1) % (train_phases + 1)),
+        )
+        for i in range(train_phases + 1)
+    ],
+    "save_epochs": 1,
+    "optimizer": torch.optim.Adam,
+    "optimizer_params": [
+        {"lr": 5e-5, "eps": 2e-4, "weight_decay": 1e-4},
+        {"lr": 5e-5, "eps": 2e-4, "weight_decay": 1e-4},
+    ],
+    "scheduler": torch.optim.lr_scheduler.StepLR,
+    "scheduler_params": {"step_size": 1, "gamma": 1.0},
+    "acc_steps": [1, 200],
+    "train_transform": torchvision.transforms.Compose(
+        [ToComplex(), SimulateMeasurements(OpA)]
+    ),
+    "val_transform": torchvision.transforms.Compose(
+        [ToComplex(), SimulateMeasurements(OpA)],
+    ),
+    "train_loader_params": {"shuffle": True, "num_workers": 8},
+    "val_loader_params": {"shuffle": False, "num_workers": 8},
+}
+
+# ----- data configuration -----
+train_data_params = {
+    "path": config.DATA_PATH,
+}
+train_data = IPDataset
+
+val_data_params = {
+    "path": config.DATA_PATH,
+}
+val_data = IPDataset
+
+# ------ save hyperparameters -------
+os.makedirs(train_params["save_path"][-1], exist_ok=True)
+with open(
+    os.path.join(train_params["save_path"][-1], "hyperparameters.txt"), "w"
+) as file:
+    for key, value in unet_params.items():
+        file.write(key + ": " + str(value) + "\n")
+    for key, value in train_params.items():
+        file.write(key + ": " + str(value) + "\n")
+    for key, value in train_data_params.items():
+        file.write(key + ": " + str(value) + "\n")
+    for key, value in val_data_params.items():
+        file.write(key + ": " + str(value) + "\n")
+    file.write("train_phases" + ": " + str(train_phases) + "\n")
+
+# ------ construct network and train -----
+unet = unet(**unet_params)
+
+# start from previously trained network
+"""
+root = ""
+param_dir = "/models/Fourier_UNet_it_no_jitter_train_phase_1/"
+file_param = "model_weights_epoch23.pt"
+params_loaded = torch.load(root + param_dir + file_param)
+unet.load_state_dict(params_loaded)
+"""
+
+if unet.device == torch.device("cpu"):
+    unet = unet.to(device)
+assert gpu_avail and unet.device == device, "for some reason unet is on %s even though gpu avail %s"%(unet.device, gpu_avail)
+# get train and validation data
+# data has shape (number of samples, (measurements, images) )
+# Note that the second dimension consist of a 2-tuple
+# image x has shape (2, N, N), since x in C^{N x N}
+# measurement y has shape (2, m) since y in C^m
+train_data = train_data("train", **train_data_params)
+val_data = val_data("val", **val_data_params)
+# run training 
+for i in range(train_phases):
+    train_params_cur = {}
+    for key, value in train_params.items():
+        train_params_cur[key] = (
+            value[i] if isinstance(value, (tuple, list)) else value
+        )
+    if i == 0:
+        # Make sure the filename corresponds to sample pattern!!!
+        #train_params_cur["fn_evolution"] = "sr_0.25_brain256_evolution"
+        train_params_cur["fn_evolution"] = "sr_0.25_ellipses256_evolution"
+        train_params_cur["plot_evolution"] = True
+    else:
+         train_params_cur["plot_evolution"] = False
+    print("Phase {}:".format(i + 1))
+    for key, value in train_params_cur.items():
+        print(key + ": " + str(value))
+    unet.train_on(train_data, val_data, **train_params_cur)
